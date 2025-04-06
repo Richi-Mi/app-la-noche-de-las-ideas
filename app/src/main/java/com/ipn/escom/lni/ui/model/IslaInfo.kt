e54f18e85ea582e5e9479aaca318b7c854e8d94b
Chris528R <crodriguezm2001@alumno.ipn.mx>
2025-04-05 22:00:40 -0600

    Info de isla

--- app/src/main/java/com/ipn/escom/lni/ui/model/IslaInfo.kt
+++ app/src/main/java/com/ipn/escom/lni/ui/model/IslaInfo.kt
@@ -5,5 +5,6 @@ import android.graphics.drawable.Drawable
 data class IslaInfo(
     val name: String,
     val image: Int,
-    val events: List<EventInfo>
+    val events: List<EventInfo>,
+    val description: String?= null
 )
--- app/src/main/java/com/ipn/escom/lni/ui/presentation/MapScreen.kt
+++ app/src/main/java/com/ipn/escom/lni/ui/presentation/MapScreen.kt
@@ -1,35 +1,44 @@
 package com.ipn.escom.lni.ui.presentation
 
-import android.graphics.drawable.VectorDrawable
-import android.util.Log
-import android.widget.Toast
+
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.drawscope.scale
 import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavController
-import androidx.navigation.NavHostController
 import com.ipn.escom.lni.NavRoutes
 import com.ipn.escom.lni.R
+import com.ipn.escom.lni.islasGlobal
+import com.ipn.escom.lni.ui.model.IslaInfo
 import kotlin.math.max
 import kotlin.math.min
-import kotlin.math.pow
-import kotlin.math.sqrt
 
 @Composable
 fun ScrollableMap(
@@ -37,7 +46,7 @@ fun ScrollableMap(
     innerPaddingValues: PaddingValues
 ) {
     val imageBitmap = ImageBitmap.imageResource(id = R.drawable.mapa) // Reemplaza con tu mapa
-
+    var showDialog by remember { mutableStateOf(false) }
 
     var offsetX by remember { mutableFloatStateOf(0f) }
     var offsetY by remember { mutableFloatStateOf(0f) }
@@ -48,14 +57,19 @@ fun ScrollableMap(
             .fillMaxSize()
             .background(Color.Transparent)
             .padding(innerPaddingValues)
-            .clickable {
-                navHostController.navigate( NavRoutes.getDetailRoute(3) )
-            }
     ) {
         AnimatedStarsBackground()
 
         Canvas(
             modifier = Modifier.fillMaxSize()
+                .pointerInput(Unit) {
+                    detectTransformGestures{  _,pan, zoom, _ ->
+                        offsetX += pan.x
+                        offsetY += pan.y
+                        scale = max(0.7f, min(scale*zoom, 2.5f))
+                    }
+                }
+                .clickable { showDialog=true }
         ) {
             val canvasWidth = size.width
             val canvasHeight = size.height
@@ -73,6 +87,79 @@ fun ScrollableMap(
             translate(left = offsetX, top = offsetY) {
                 scale(scale * initialScale, pivot = pivot) {
                     drawImage(imageBitmap)
+                }
+            }
+        }
+        if (showDialog){
+            DialogUtopia(
+                onDismiss = { showDialog = false },
+                onViewClick = {
+                    showDialog = false
+                    navHostController.navigate( NavRoutes.getDetailRoute(3) )
+                },
+                islasGlobal[3]
+            )
+        }
+    }
+}
+
+@Composable
+fun DialogUtopia(
+    onDismiss: () -> Unit,
+    onViewClick: () -> Unit,
+    isla: IslaInfo
+) {
+    Dialog(
+        onDismissRequest = { onDismiss() }
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(16.dp)
+                .clip(RoundedCornerShape(24.dp))
+                .background(MaterialTheme.colorScheme.secondaryContainer)
+                .padding(24.dp)
+        ) {
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Row (modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween){
+                    Text(
+                        text = isla.name,
+                        style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Bold),
+                        modifier = Modifier.weight(1f)
+                    )
+                    IconButton(onClick = onDismiss) {
+                        Icon(Icons.Default.Clear, contentDescription = "Cerrar")
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Image(
+                    painter = painterResource( id = isla.image),
+                    contentDescription = "Imagen de isla",
+                    modifier = Modifier
+                        .fillMaxWidth(0.7f)
+                        .clip(RoundedCornerShape(24.dp)),
+                    contentScale = ContentScale.Fit
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Text(
+                    text = isla.description ?: "",
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Button(
+                    onClick = onViewClick,
+                    colors = ButtonDefaults.buttonColors(contentColor = MaterialTheme.colorScheme.tertiaryContainer)
+                ) {
+                    Text("Ver", color = MaterialTheme.colorScheme.onTertiary)
                 }
             }
         }
